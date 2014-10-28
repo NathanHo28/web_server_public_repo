83863c99752921251510a501686a8f52111c5d06
NATHAN <nathan.ho28@gmail.com>
2014-10-27 22:09:16 -0400

    variable name changes and html/css additions

--- server.rb
+++ server.rb
@@ -6,61 +6,57 @@ port = 2000
 server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
 puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
 
-loop do                                             # Server runs forever
-  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
+loop do    
+                                         # Server runs forever
+	client = server.accept                          # Wait for a client to connect. Accept returns a TCPSocket
 
-  lines = []
-  while (line = client.gets)
-  line = line.chomp
-  break if line.empty?  							# Read the request and collect it until it's empty
-    lines << line
-  end
-  puts lines          								# Output the full request to stdout
-
-  filename = lines[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '')  #use rubular to test regular expressions.
-  headers = []
-
-  puts filename
-
-  if File.exists?(filename)
-  	body = File.read(filename)
-  	headers << "HTTP/1.1 200 OK"
-
-  	if filename =~ /.css/
-  		filetype = "text/css"
-  	elsif filename =~ /.html/
-  		filetype = "text/html"
-  	else
-  		filetype = "text/plain"
+	request_headers = []
+	while (line = client.gets)
+		line.chomp!
+  		break if line.empty?                        # Read the request and collect it until it's empty
+  		request_headers << line
   	end
-  		
-  	headers << "Content-Type: #{filetype}"
+ 	puts request_headers                                      # Output the full request to stdout
+
+ 	filename = request_headers[0].gsub(/GET \//, '').gsub(/\ HTTP.*/, '') #strips everything but the file extension.
+
+ 	response_headers = [] #-->These are the headers (in an arrray) which we give back to the browswer.
+
+ 	if File.exists?(filename)
+ 		response_headers << "HTTP/1.1 200 OK" #-->Telling the browser it's okay if this file is found
+ 		response = File.read(filename)
+ 		if filename =~ /.css/		#unsure what the tilde does in this situation...
+ 			filetype = "text/css"
+ 		elsif filename =~ /.html/
+ 			filetype = "text/html"
+ 		elsif filename =~ /.png/
+ 			filetype = "image/png"
+ 				
+ 		else 
+ 			filetype = "text/plain"
+ 		end
+
+ 	response_headers << "Content-Type: #{filetype}" #-->
+ 		
+ 	else
+ 		response_headers << "HTTP/1.1 404 Not Found"
+ 		response_headers << "Content-Type: text/plain"
+ 		response = "File Not Found"
+ 	end
+
+	response_headers << "Content-Length: #{response.size}"
+	response_headers << "Connection: close"
  
-  else
-  	headers << "HTTP/1.1 404 Not Found"
-  	headers << "Content-Type: text/plain"
-  	body = "File not found\n"
-  end
+	headers = response_headers.join("\r\n") 
 
-  headers << "Content-Length: #{body.size}"
-  headers << "Connection: close"
+	puts
+	puts "HEADERS: #{headers}"
 
-  # headers = ""
-  # headers += "HTTP/1.1 200 OK\r\n"
-  # headers += "Content-Type: text/plain\r\n"
-  # headers += "Content-Length: #{body.size}\r\n"  
-  # headers += "Connection: close\r\n"  
+	result = [headers, response].join("\r\n\r\n")
 
-	# headers = [
-	#   	"HTTP/1.1 200 OK",
-	#   	"Content-Type: text/plain",
-	#   	"Content-Length: #{body.size}",
-	#   	"Connection: close"
- #  	]    
+	puts
+	puts "RESULT: #{result}"
 
-	headers = headers.join("\r\n")
-	response = [headers, body].join("\r\n\r\n")     
-
-  client.puts(response)                       		# Output the current time to the client
-  client.close                                      # Disconnect from the client
+ 	client.puts(result)
+  	client.close                                      # Disconnect from the client
 end
